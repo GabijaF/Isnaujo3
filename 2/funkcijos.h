55efebf16ebed1ee4f343f05ee01ec6a5e889e70
GabijaF <145053488+GabijaF@users.noreply.github.com>
2023-12-11 23:35:13 +0200

    Add files via upload

---
+++ 2/funkcijos.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+
+struct Studentas {
+    std::string vardas;
+    std::string pavarde;
+    std::vector<int> Nd;
+    int egz;
+    Studentas* address;
+};
+
+int generuotiNdBala();
+int generuotiEgzBala();
+double skaiciuotiVidurki(const std::vector<int>& pazymiai);
+double skaiciuotiMediana(const std::vector<int>& pazymiai);
+double skaiciuotiGalutiniBala(const Studentas& studentas, char baloSkaiciavimoBudas);
