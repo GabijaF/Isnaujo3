2014e7b991f468874b9a89e59db9ec2ddd6e2b71
GabijaF <145053488+GabijaF@users.noreply.github.com>
2023-12-15 00:24:25 +0200

    Update and rename main.cpp to 3/algoritmai/remove_copy_if.cpp

--- 3/algoritmai/remove_copy_if.cpp
+++ 3/algoritmai/remove_copy_if.cpp
@@ -6,7 +6,6 @@
 #include <iterator>
 
 
-
 int main() {
     std::vector<Studentas> studentai;
     std::vector<Studentas> vargsiukai;
@@ -16,12 +15,12 @@ int main() {
     std::cout << "Ar norite ivesti duomenis (R) ar skaityti is failo (F)? ";
     std::cin >> pasirinkimas;
 
-    //auto startReading = std::chrono::high_resolution_clock::now();
+    
 
     if (pasirinkimas == 'R' || pasirinkimas == 'r') {
         Studentas naujasStudentas;
         naujasStudentas.address = &naujasStudentas;
-        //std::cout << "adresas:" << &naujasStudentas << std::endl;
+        std::cout << "adresas:" << &naujasStudentas << std::endl;
 
         do {
             std::cout << "Iveskite studento varda: ";
@@ -84,7 +83,7 @@ int main() {
             return galutinisBalas >= 5.0;
         });
 
-        // Kopijuojame galvocius
+        // Kopijuoja galvocius
         std::remove_copy_if(studentai.begin(), studentai.end(), std::back_inserter(galvociai), [&](const Studentas& studentas) {
             double galutinisBalas = skaiciuotiGalutiniBala(studentas, baloSkaiciavimoBudas);
             return galutinisBalas < 5.0;
@@ -106,10 +105,10 @@ int main() {
                 return skaiciuotiGalutiniBala(a, baloSkaiciavimoBudas) > skaiciuotiGalutiniBala(b, baloSkaiciavimoBudas);
             };
 
-            // Sorting vargsiukai
+            
             std::sort(vargsiukai.begin(), vargsiukai.end(), compareByFinalGrade);
 
-            // Sorting galvociai
+            
             std::sort(galvociai.begin(), galvociai.end(), compareByFinalGrade);
         }
 
@@ -135,7 +134,7 @@ int main() {
         std::cout << "Failo isvedimas i galvociai uztruko: " << writeGalvociaiTime.count() << " sekundes" << std::endl;
 
 
-        // Output table for galvociai in the terminal
+        
         auto startWriteVargsiukaiTime = std::chrono::high_resolution_clock::now();
 
         std::ofstream vargsiukaiFile("vargsiukai.txt");
@@ -166,9 +165,6 @@ int main() {
             char baloSkaiciavimoBudas;
             std::cout << "Kaip norite skaiciuoti galutini bala pagal vidurki (V) ar pagal mediana (M)? ";
             std::cin >> baloSkaiciavimoBudas;
-
-
-
 
 
             auto startReadFileTime = std::chrono::high_resolution_clock::now();
@@ -206,7 +202,6 @@ int main() {
                 return galutinisBalas >= 5.0;
             });
 
-            // Kopijuojame galvocius
             std::remove_copy_if(studentai.begin(), studentai.end(), std::back_inserter(galvociai), [&](const Studentas& studentas) {
                 double galutinisBalas = skaiciuotiGalutiniBala(studentas, baloSkaiciavimoBudas);
                 return galutinisBalas < 5.0;
@@ -228,10 +223,10 @@ int main() {
                     return skaiciuotiGalutiniBala(a, baloSkaiciavimoBudas) > skaiciuotiGalutiniBala(b, baloSkaiciavimoBudas);
                 };
 
-                // Sorting vargsiukai
+                
                 std::sort(vargsiukai.begin(), vargsiukai.end(), compareByFinalGrade);
 
-                // Sorting galvociai
+                
                 std::sort(galvociai.begin(), galvociai.end(), compareByFinalGrade);
             }
 
@@ -241,14 +236,6 @@ int main() {
 
 
             std::cout << "Failo su " << studentCount << " studentu dalijimas i 2 grupes uztruko: " << sortTime.count() << " sekundes" << std::endl;
-
-
-
-
-
-
-
-
 
 
 
@@ -288,11 +275,6 @@ int main() {
         std::cerr << "Neteisingas pasirinkimas." << std::endl;
         return 1;
     }
-
-    //auto endReading = std::chrono::high_resolution_clock::now();
-    //auto durationReading = std::chrono::duration_cast<std::chrono::seconds>(endReading - startReading);
-
-    //std::cout << "Viso programa uztruko: " << durationReading.count() << " sekundes" << std::endl;
 
     return 0;
 }
