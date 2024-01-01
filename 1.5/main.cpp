9b689b87c5a6f9c8c583e94323590ee5e59fc3ce
GabijaF <145053488+GabijaF@users.noreply.github.com>
2024-01-01 20:27:39 +0200

    Update main.cpp

--- 1.5/main.cpp
+++ 1.5/main.cpp
@@ -1,1 +1,40 @@
 
+
+#include <iostream>
+
+int vektoriai();
+int listai();
+void Demonstracija3();
+
+int main() {
+    char choice;
+    do {
+        std::cout << "Pasirinkite tipa (vektorius(1) arba listas(2) arba taisykles pvz(3)): ";
+        std::cin >> choice;
+
+        if (choice == '1') {
+            // Execute vektoriai.cpp
+            vektoriai();  // Replace with the actual command for execution
+        } else if (choice == '2') {
+            // Execute listai.cpp
+            listai();  // Replace with the actual command for execution
+        } else if (choice == '3') {
+            // Execute listai.cpp
+            Demonstracija3(); // Replace with the actual command for execution
+        } else {
+            std::cout << "Neteisingas pasirinkimas." << std::endl;
+        }
+        std::cout << "\n";
+        std::cout << "\n";
+        std::cout << "Ar norite kartoti programa? (y/n): ";
+        std::cin >> choice;
+
+
+    } while (choice == 'y' || choice == 'Y');
+
+    std::cout << "einam lauk" << std::endl;
+
+    return 0;
+}
+
+
